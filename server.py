7375fd5defc117dad416fa520800502379d2f86c
tarxvrf <166564641+tarxvrf@users.noreply.github.com>
2024-07-09 22:33:48 +0700

    Update server.py
    perbaikan code

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 sock= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host='192.168.1.7'
+host='diisiIPserverkita'
 port=7777
 sock.bind((host,port))
 print('menunggu koneksi ...')
